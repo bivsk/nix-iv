6fc0c3b52ad4f63394727dcc3f2a6e7b46a187d9
bivsk <bivsk@tutanota.com>
2025-04-15 03:28:56 -0400

    fonts: replace comic mono with comicshanns nf

--- home/default.nix
+++ home/default.nix
@@ -95,10 +95,11 @@
 
   programs.ghostty = {
     enable = true;
-    package = inputs.ghostty.packages.${pkgs.system}.default.override { enableX11 = false; }; # latest
-    #enableBashIntegration = true;
+    package = inputs.ghostty.packages.${pkgs.system}.default.override { enableX11 = false; };
+    enableBashIntegration = true;
     installBatSyntax = false;
     settings = {
+      font-family = "ComicShannsMono Nerd Font Mono";
       theme = "Solarized Dark - Patched";
       background-opacity = 0.93;
       window-decoration = false;
--- system/desktop/fonts.nix
+++ system/desktop/fonts.nix
@@ -5,7 +5,6 @@
     packages = with pkgs; [
       # icons
       material-symbols
-      nerd-fonts.symbols-only
 
       # Sans(Serif)
       comic-neue
@@ -15,14 +14,15 @@
       noto-fonts-emoji
       roboto
 
-      # monospace
-      comic-mono
+      # nerd fonts
+      nerd-fonts.comic-shanns-mono
+      nerd-fonts.symbols-only
     ];
 
     fontconfig.defaultFonts = {
       serif = ["Libertinus Serif"];
       sansSerif = ["Comic Neue"];
-      monospace = ["Comic Mono"];
+      monospace = ["ComicShannsMono Nerd Font Mono"];
       emoji = ["Noto Color Emoji"];
     };
   };
