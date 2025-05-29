fcf7796eda6fcae09785e3b2230cce347894b912
bivsk <bivsk@tutanota.com>
2025-05-28 20:23:40 -0400

    rust: initial commit

---
+++ modules/rust.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }: let
+  inherit (lib) attrValues;
+in {
+  environment.variables = {
+    CARGO_NET_GIT_FETCH_WITH_CLI = "true";
+  };
+
+  environment.systemPackages = attrValues {
+    inherit (pkgs)
+      cargo-deny
+      cargo-expand
+      cargo-fuzz
+      cargo-nextest
+
+      evcxr
+
+      taplo
+    ;
+
+    fenix = pkgs.fenix.complete.withComponents [
+      "cargo"
+      "clippy"
+      "rust-src"
+      "rustc"
+      "rustfmt"
+    ];
+  };
+}
