f7c543456e9c2faca8743d3e1eedf598be0b46e1
bivsk <bivsk@tutanota.com>
2025-05-28 20:48:23 -0400

    gammastep: initial commit

---
+++ modules/hyprland/hyprland.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }: let
+  inherit (lib) attrValues enabled flatten merge mkIf range;
+in merge <| mkIf config.isDesktop {
+  hardware.graphics = enabled;
+
+  services.logind.powerKey = "ignore";
+
+  xdg.portal = enabled {
+    config.common.default = "*";
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+
+    configPackages = [
+      pkgs.hyprland
+    ];
+
+    programs.xwayland = enabled;
+
+    environment.systemPackages = attrValues {
+      inherit (pkgs)
+        brightnessctl
+	grim
+	hyprpicker
+	slurp
+	swappy
+	swaybg
+	wl-clipboard
+	wtype
+	xdg-utils
+	xwaylandvideobridge
+      ;
+    };
+
+    home-manager.sharedModules = [{
+      wayland.windowManager.hyprland = enabled {
+        systemd = enabled {
+	  # enableXdgAutostart = true;
+	};
+
+	# settings = {};
+      };
+    }];
+  };
+}
