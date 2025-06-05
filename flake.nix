175e9a66cb0517bd81b4e91f1f3ea0623dfaf423
bivsk <bivsk@tutanota.com>
2025-06-05 02:23:23 -0400

    flake.nix: remove lazy-trees until switch to determinate nix

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     builders-use-substitutes = true;
     flake-registry = "";
     http-connections = 50;
-    lazy-trees = true;
+    # lazy-trees = true; # determinate nix
     show-trace = true;
     trusted-users = [ "root" "@build" "@wheel" "@admin" ];
     use-cgroups = true;
