6f0d155ac0a20b5a5d30a42dbdf629daa403cc33
bivsk <bivsk@tutanota.com>
2025-06-05 00:16:34 -0400

    keyring.nix: exclude servers

--- modules/keyring.nix
+++ modules/keyring.nix
@@ -1,6 +1,6 @@
-{ lib, ... }: let
-  inherit (lib) enabled;
-in {
+{ config, lib, ... }: let
+  inherit (lib) enabled merge mkIf;
+in merge <| mkIf config.isDesktop {
   programs.seahorse = enabled;
 
   security.pam.services.login.enableGnomeKeyring = true;
