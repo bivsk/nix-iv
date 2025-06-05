a16eb7cb2f3308be1882e936d9d6b8b00c71752f
bivsk <bivsk@tutanota.com>
2025-06-04 23:59:34 -0400

    hyprland: brightness keybinds

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -45,6 +45,8 @@ in merge <| mkIf config.isDesktop {
 	bindle = [
 	  ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+ --limit 1.25"
 	  ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+	  ",XF86MonBrightnessUp, exec, brillo -A 5"
+	  ",XF86MonBrightnessDown exec, brillo -U 5"
 	];
 
 	bindl = [
