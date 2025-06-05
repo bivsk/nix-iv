8eba9a8adb61ee0d795c3601015e0022eb3d8070
bivsk <bivsk@tutanota.com>
2025-06-04 23:58:52 -0400

    power.nix: enable basic power management

---
+++ modules/power.nix
@@ -0,0 +1,7 @@
+{ config, lib, ... }: let
+  inherit (lib) enabled merge mkIf;
+in merge <| mkIf config.isDesktop {
+  powerManagement = enabled;
+  services.power-profiles-daemon = enabled;
+  services.upower = enabled;
+}
