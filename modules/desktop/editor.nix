d02d651c5a4dd8d9f078b1ccdd009b1d61a711d7
bivsk <bivsk@tutanota.com>
2025-06-05 23:24:14 -0400

    remove zed-editor

--- modules/desktop/editor.nix
+++ modules/desktop/editor.nix
@@ -13,30 +13,6 @@ in merge <| mkIf config.isDesktop {
       package = pkgs.vscodium;
     };
 
-    programs.zed-editor = enabled {
-      extensions = ["nix"];
-	  #    userSettings = {
-	  #      assistant.enabled = false;
-	  #
-	  #      node = {
-	  #        path = lib.getExe pkgs.nodejs;
-	  #        npm_path = lib.getExe' pkgs.nodejs "npm";
-	  #      };
-	  #
-	  #      hour_format = "hour24";
-	  #      auto_update = false;
-	  #
-	  #      lsp = {
-	  #        rust-analyzer = {
-	  #          binary = {
-	  #            path = lib.getExe pkgs.rust-analyzer;
-	  #            path_lookup = true;
-	  #          };
-	  # };
-	  #      };
-	  #    };
-    };
-
     # programs.emacs = enabled {
     #   # client.enable = true;
     #   defaultEditor = true;
