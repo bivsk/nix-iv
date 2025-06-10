0bda79d10e3fdee5d6ea439cdb41a4d6cad6c6b6
bivsk <bivsk@tutanota.com>
2025-06-10 17:16:07 -0400

    btop: install btop-rocm on desktops

--- modules/misc/btop.nix
+++ modules/misc/btop.nix
@@ -1,12 +1,13 @@
-{ config, lib, ... }: let
+{ config, lib, pkgs, ... }: let
   inherit (lib) enabled;
 in {
   home-manager.sharedModules = [{
     xdg.configFile."btop/themes/base16.theme".text = config.theme.btopTheme;
 
     programs.btop = enabled {
-      settings.color_theme = "base16";
+      package = if config.isDesktop then pkgs.btop else pkgs.btop-rocm;
 
+      settings.color_theme = "base16";
       settings.rounded_corners = config.theme.cornerRadius > 0;
     };
   }];
