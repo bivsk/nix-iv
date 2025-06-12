aa90cabe354ad060ba496192b093194af757b48b
bivsk <bivsk@tutanota.com>
2025-06-12 18:06:41 -0400

    packages: split user packages from system

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,11 +3,8 @@
 in {
   environment.systemPackages = attrValues <| {
     inherit (pkgs)
-      asciinema
-      cowsay
       curlHTTP3
       dig
-      fastfetch
       fd
       htop
       hyperfine
@@ -18,17 +15,13 @@ in {
       rsync
       strace
       timg
-      tokei
       traceroute
       tree
       usbutils
       uutils-coreutils-noprefix
       wgetpaste
-      yazi
-      yt-dlp
     ;
 
-    fortune = pkgs.fortune.override { withOffensive = true; };
   } // optionalAttrs config.isDesktop {
     inherit (pkgs)
       # toolchain
@@ -39,21 +32,47 @@ in {
       lld
       mold
       zig
-
-      # TODO: move to home-manager
-      # gfx
-      gimp
-      krita
-      libreoffice
-
-      # social
-      element-desktop
-      telegram-desktop
-      qbittorrent
     ;
 
     inherit (pkgs.hunspellDicts)
       en_US
     ;
   };
+
+  home-manager.sharedModules = [{
+    home.packages = attrValues <| {
+      inherit (pkgs)
+        asciinema
+	cowsay
+	fastfetch
+	tokei
+	yazi
+	yt-dlp
+      ;
+      fortune = pkgs.fortune.override { withOffensive = true; };
+    } // optionalAttrs config.isDesktop {
+      inherit (pkgs)
+        # gfx
+	gimp
+	krita
+
+	# office
+	libreoffice
+	tradingview
+
+	# social
+	element-desktop
+	telegram-desktop
+
+	# crypto wallets
+	ledger-live-desktop
+	monero-gui
+	trezor-suite
+	
+	# net
+	qbittorrent
+        ;
+        tari-universe = self.packages.x86_64-linux.tari-universe;
+    };
+  }];
 }
