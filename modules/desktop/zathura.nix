36aa92698d5fde6a21d47164a04ec53c35307921
bivsk <bivsk@tutanota.com>
2025-06-14 00:42:30 -0400

    gotta read some pdfs

---
+++ modules/desktop/zathura.nix
@@ -0,0 +1,12 @@
+{ config, lib, ... }: let
+  inherit (lib) enabled merge mkIf;
+in merge <| mkIf config.isDesktop {
+  home-manager.sharedModules = [{
+    programs.zathura = with config.theme.withHashtag; enabled {
+      options = {
+        default-bg = "${base00}";
+        default-fg = "${base07}";
+      };
+    };
+  }];
+}
