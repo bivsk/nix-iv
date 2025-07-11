98e4e3216e15831dac00bafb8b826bda319c1dad
bivsk <bivsk@tutanota.com>
2025-07-10 20:12:05 -0400

    home-manager: optionally import a host-specific home-manager module.

--- modules/home-manager/nixos.nix
+++ modules/home-manager/nixos.nix
@@ -1,29 +1,34 @@
 {
-  config,
   inputs,
+  config,
   ...
 }:
 {
-  flake.modules.nixos.pc = {
-    imports = [ inputs.home-manager.nixosModules.home-manager ];
+  flake.modules.nixos.pc = 
+    let
+      inherit (config.networking) hostName;
+    in
+    {
+      imports = [ inputs.home-manager.nixosModules.home-manager ];
 
-    home-manager = {
-      useGlobalPkgs = true;
-      extraSpecialArgs.hasGlobalPkgs = true;
-      # https://github.com/nix-community/home-manager/issues/6770
-      #useUserPackages = true;
-      backupFileExtension = "bak";
+      home-manager = {
+        useGlobalPkgs = true;
+        extraSpecialArgs.hasGlobalPkgs = true;
+        # https://github.com/nix-community/home-manager/issues/6770
+        #useUserPackages = true;
+        backupFileExtension = "bak";
 
-      users.${config.flake.meta.owner.name}.imports = [
-        (
-          { osConfig, ... }:
-          {
-            home.stateVersion = osConfig.system.stateVersion;
-          }
-        )
-        config.flake.modules.homeManager.base
-        config.flake.modules.homeManager.gui
-      ];
+        users.${config.flake.meta.owner.name}.imports = [
+          (
+            { osConfig, ... }:
+            {
+              home.stateVersion = osConfig.system.stateVersion;
+            }
+          )
+          config.flake.modules.homeManager.base
+          config.flake.modules.homeManager.gui
+	  (config.flake.modules.homeManager."homeConfigurations/kagome" or { })
+        ];
+      };
     };
-  };
 }
