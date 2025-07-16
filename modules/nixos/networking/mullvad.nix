49687227795562b7b4f304475c7d7ec63e84b43f
bivsk <bivsk@tutanota.com>
2025-07-16 03:09:28 -0400

    mullvad: remove default appconfig

--- modules/nixos/networking/mullvad.nix
+++ modules/nixos/networking/mullvad.nix
@@ -8,12 +8,6 @@
           mullvad-vpn = {
             package = lib.mkForce pkgs.mullvad-vpn; # gui
           };
-          geoclue2.appConfig = {
-            # reduce network location accuracy
-            networkAccuracy = 10000; # meters
-            # ignore vpn
-            ignoredNetworkInterfaces = [ "wg0-mullvad" ];
-          };
         };
       };
   };
