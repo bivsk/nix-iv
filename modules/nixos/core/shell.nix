c6cdf1eb966e637098a0f819a8fcd7ee4a546cc1
bivsk <bivsk@tutanota.com>
2025-07-16 02:21:53 -0400

    nixos/core: enable crash as default shell

--- modules/nixos/core/shell.nix
+++ modules/nixos/core/shell.nix
@@ -1,7 +1,9 @@
+{ inputs, ... }:
 {
   flake.modules.nixos.core = 
     { pkgs, ... }:
     {
+      nixpkgs.overlays = [ inputs.crash.overlays.default ];
       users.defaultUserShell = pkgs.crash;
     };
 }
