93061d6b7a500704b6b43488cd3352df59bb43b9
bivsk <bivsk@tutanota.com>
2025-07-21 22:41:46 -0400

    agenix: automatically add rekeyed secrets to git

--- modules/flake/agenix.nix
+++ modules/flake/agenix.nix
@@ -15,8 +15,18 @@
       ...
     }:
     {
-      devshells.default.packages = [
-        inputs'.agenix-rekey.packages.default
-      ];
+      devshells.default = {
+        packages = [
+          inputs'.agenix-rekey.packages.default
+        ];
+	env = [
+	  {
+	    # Automatically add rekeyed secrets to git without
+	    # requiring `agenix rekey -a`
+	    name = "AGENIX_REKEY_ADD_TO_GIT";
+	    value = true;
+	  }
+	];
+      };
     };
 }
