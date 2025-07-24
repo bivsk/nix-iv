1dbf0ee7e76585342625054da6f7ec187dc55606
bivsk <bivsk@tutanota.com>
2025-07-24 04:03:09 -0400

    hyprland: play/pause/skip via swayosd

--- modules/home/desktop/wayland/swayosd.nix
+++ modules/home/desktop/wayland/swayosd.nix
@@ -3,13 +3,21 @@
     services.swayosd.enable = true;
 
     wayland.windowManager.hyprland.settings = {
+      "$osdclient" = "swayosd-client --monitor \"$(hyprctl monitors -j | jq -r '.[] | select(.focused == true).name')\"";
+
       bindle = [
-        ", XF86AudioRaiseVolume, exec, swayosd-client --output-volume raise"
-        ", XF86AudioLowerVolume, exec, swayosd-client --output-volume lower"
-        ", XF86AudioMute, exec, swayosd-client --output-volume mute-toggle"
-        ", XF86AudioMicMute, exec, swayosd-client --input-volume mute-toggle"
-        ", XF86MonBrightnessUp, exec, swayosd-client --brightness raise"
-        ", XF86MonBrightnessDown, exec, swayosd-client --brightness lower"
+        ", XF86AudioRaiseVolume, exec, $osdclient --output-volume raise"
+        ", XF86AudioLowerVolume, exec, $osdclient --output-volume lower"
+        ", XF86AudioMute, exec, $osdclient --output-volume mute-toggle"
+        ", XF86AudioMicMute, exec, $osdclient --input-volume mute-toggle"
+        ", XF86MonBrightnessUp, exec, $osdclient --brightness raise"
+        ", XF86MonBrightnessDown, exec, $osdclient --brightness lower"
+      ];
+      bindl = [
+        ", XF86AudioNext, exec, $osdclient --playerctl next"
+        ", XF86AudioPause, exec, $osdclient --playerctl play-pause"
+        ", XF86AudioPlay, exec, $osdclient --playerctl play-pause"
+        ", XF86AudioPrev, exec, $osdclient --playerctl previous"
       ];
     };
   };
