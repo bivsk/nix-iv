57a20de5567437a7b6046c5f7e4d4a897760026c
bivsk <bivsk@tutanota.com>
2025-07-27 23:54:37 -0400

    sunny: specify proper ip

--- modules/hosts/sunny/configuration.nix
+++ modules/hosts/sunny/configuration.nix
@@ -6,7 +6,7 @@
       age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDG/u5Gu1JiCGK3NPAPNX9oMIxA6bHe4Fc3W0drS/rOj";
 
       networking.hostName = "sunny";
-      networking.ipv4.address = "192.168.0.29";
+      networking.ipv4.address = "192.168.0.22";
 
       boot.loader.timeout = 3;
 
