2b4612fdcfc9a63acd81c6523a3ff97c308b41f5
bivsk <bivsk@tutanota.com>
2025-08-03 00:09:36 -0400

    nixarr: expose sonarr

--- modules/nixos/server/nixarr/default.nix
+++ modules/nixos/server/nixarr/default.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.nixarr = 
+  flake.modules.nixos.nixarr =
     { config, inputs, ... }:
     {
       imports = [ inputs.nixarr.nixosModules.default ];
@@ -7,48 +7,46 @@
       nixarr = {
         enable = true;
         # cannot be under any non-root owned dir
-	mediaDir = "/data/media";
-	stateDir = "/data/media/.state/nixarr";
+        mediaDir = "/data/media";
+        stateDir = "/data/media/.state/nixarr";
       };
-      
+
       secrets.nixarr-vpn-conf.rekeyFile = ./nixarr-vpn-conf.age;
       nixarr.vpn = {
         enable = true;
-	wgConf = config.secrets.nixarr-vpn-conf.path; 
-	vpnTestService.enable = true;
-	accessibleFrom = [
-	  "10.0.0.0/24"
-	];
+        wgConf = config.secrets.nixarr-vpn-conf.path;
+        vpnTestService.enable = true;
+        accessibleFrom = [
+          "10.0.0.0/24"
+        ];
       };
 
       # modules
       nixarr = {
         bazarr.enable = true;
-	prowlarr.enable = true;
+        prowlarr.enable = true;
         radarr.enable = true;
-	readarr.enable = true;
-	readarr-audiobook.enable = true;
-        sonarr.enable = true;
+        readarr.enable = true;
+        readarr-audiobook.enable = true;
+        sonarr = {
+          enable = true;
+          openFirewall = true;
+        };
 
-	jellyfin = {
-	  enable = true;
-	  openFirewall = true;
-	};
-	jellyseerr = {
-	  enable = true;
-	  openFirewall = true;
-	};
+        jellyfin = {
+          enable = true;
+          openFirewall = true;
+        };
+        jellyseerr = {
+          enable = true;
+          openFirewall = true;
+        };
 
         transmission = {
-	  enable = true;
-	  vpn.enable = true;
-	  peerPort = 58420;
-	};
-      };
-
-      services.jackett = {
-        enable = true;
-	dataDir = "/data/media/.state/jackett";
+          enable = true;
+          vpn.enable = true;
+          peerPort = 58420;
+        };
       };
     };
 }
