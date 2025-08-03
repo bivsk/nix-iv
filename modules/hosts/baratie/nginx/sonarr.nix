d2d8a68343973168f99ff480618a0c0cdf574124
bivsk <bivsk@tutanota.com>
2025-08-03 00:26:11 -0400

    chore: update flake.lock

---
+++ modules/hosts/baratie/nginx/sonarr.nix
@@ -0,0 +1,22 @@
+{
+  flake.modules.nixos.nginx-sonarr = {
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."sonarr.bivsk.com" = {
+        forceSSL = true;
+        enableACME = true;
+
+        extraConfig = ''
+          	  proxy_set_header Referer $http_referer;
+          	  proxy_set_header X-Real-Port $remote_port;
+          	  proxy_set_header X-Forwarded-Ssl on;
+          	'';
+
+        locations."/" = {
+          proxyPass = "http://10.0.0.2:8989";
+        };
+      };
+    };
+  };
+}
