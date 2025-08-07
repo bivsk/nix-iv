1a7631a8ca68ab4d455e6d5645fe59de5f6e7841
bivsk <bivsk@tutanota.com>
2025-08-06 21:08:02 -0400

    nix: remove unneeded home-manager module

--- modules/nixos/core/nix/determinate.nix
+++ modules/nixos/core/nix/determinate.nix
@@ -1,20 +1,11 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.nix =
-    { lib, pkgs, ... }:
-    {
-      imports = [ inputs.determinate.nixosModules.default ];
+  flake.modules.nixos.nix = {
+    imports = [ inputs.determinate.nixosModules.default ];
 
-      nix.settings = {
-        substituters = [ "https://install.determinate.systems" ];
-        trusted-public-keys = [ "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM" ];
-        lazy-trees = true;
-      };
-
-      # home-manager.sharedModules = [
-      #   {
-      #     nix.package = lib.mkForce inputs.nix.packages.${pkgs.stdenv.system}.default;
-      #   }
-      # ];
+    nix.settings = {
+      substituters = [ "https://install.determinate.systems" ];
+      trusted-public-keys = [ "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM" ];
     };
+  };
 }
