a73a1033d8f75a24b5f626760f7c71c9ceccaa6a
bivsk <bivsk@tutanota.com>
2025-08-13 02:06:19 -0400

    baratie: consider some wireguard key improvements

--- modules/hosts/baratie/wireguard/default.nix
+++ modules/hosts/baratie/wireguard/default.nix
@@ -5,17 +5,18 @@
       port = 58440;
     in
     {
+      # TODO: maybe just generate a system key into /persist?
       secrets.wireguard-private-key.rekeyFile = ./private-key.age;
 
-      # Open wireguard port in firewall
       networking.firewall.allowedUDPPorts = [ port ];
 
-      # Configure wg interface
       networking.wireguard.interfaces = {
         wg0 = {
           ips = [ "10.0.0.1/24" ];
           listenPort = port;
           privateKeyFile = config.secrets.wireguard-private-key.path;
+          # privateKeyFile = /persist/wg/private.key
+	  # should /persist be portable between machines?
           peers = [
             {
               publicKey = "mY78JydE1dLOiTG6fIxeS0lckU/uAJ/WN4d8Q0aBeDI=";
