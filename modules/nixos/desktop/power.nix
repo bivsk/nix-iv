44eed10e09a7b1e6a24ff3121f807c9e9b21957a
bivsk <bivsk@tutanota.com>
2025-08-28 20:13:01 -0400

    power: enable powertop

--- modules/nixos/desktop/power.nix
+++ modules/nixos/desktop/power.nix
@@ -1,6 +1,7 @@
 {
   flake.modules.nixos.power-management = {
     powerManagement.enable = true;
+    powerManagement.powertop.enable = true;
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
   };
