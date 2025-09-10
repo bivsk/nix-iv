13613560e6d3e4efcd053e4b72bb081df6f505e9
bivsk <bivsk@tutanota.com>
2025-09-09 23:50:13 -0400

    ssh: generate sys keys to /persist

--- modules/nixos/core/network/ssh/sshd.nix
+++ modules/nixos/core/network/ssh/sshd.nix
@@ -12,6 +12,19 @@
         enable = true;
         ports = [ port ];
 
+	# TODO: make sure impermanence is enabled
+        hostKeys = [
+          {
+            path = "/persist/etc/ssh/ssh_host_ed25519_key";
+            type = "ed25519";
+          }
+          {
+            path = "/persist/etc/ssh/ssh_host_rsa_key";
+            type = "rsa";
+            bits = 4096;
+          }
+        ];
+
         settings = {
           # TODO: lockdown
           KbdInteractiveAuthentication = true;
