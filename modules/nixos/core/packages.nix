7b26d3190d2c491f7a96045e06819f6d30abd7a8
bivsk <bivsk@tutanota.com>
2025-09-12 02:30:01 -0400

    core: customize htop

--- modules/nixos/core/packages.nix
+++ modules/nixos/core/packages.nix
@@ -7,7 +7,6 @@
           dig
           file
           git
-          htop
           killall
           lsof
           nfs-utils
@@ -19,6 +18,18 @@
           wget
           zip
           ;
+      };
+
+      programs.htop = {
+        enable = true;
+        settings = {
+          highlight_base_name = 1;
+	  show_cpu_temperature = 1;
+	  column_meters_0 = "LeftCPUs2 GPU Memory Swap ZFSARC";
+	  column_meter_modes_0 = "1 1 1 1 2";
+	  column_meters_1 = "RightCPUs2 Tasks LoadAverage Uptime Hostname";
+	  column_meter_modes_1 = "1 2 2 2 2";
+        };
       };
     };
 }
