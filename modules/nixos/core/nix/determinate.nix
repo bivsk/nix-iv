05750e197cd8e65b2d5009fb7f5eac0dd00ef3ed
bivsk <bivsk@tutanota.com>
2025-09-12 00:01:02 -0400

    determinate: enable parallel eval

--- modules/nixos/core/nix/determinate.nix
+++ modules/nixos/core/nix/determinate.nix
@@ -4,6 +4,8 @@
     imports = [ inputs.determinate.nixosModules.default ];
 
     nix.settings = {
+      eval-cores = 0;
+
       substituters = [ "https://install.determinate.systems" ];
       trusted-public-keys = [ "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM" ];
     };
