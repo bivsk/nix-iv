a4e71a69ecb4db1cb717655689436195e49b6a55
bivsk <bivsk@tutanota.com>
2025-09-14 15:52:43 -0400

    chore: formatting

--- modules/nixos/core/packages.nix
+++ modules/nixos/core/packages.nix
@@ -24,11 +24,11 @@
         enable = true;
         settings = {
           highlight_base_name = 1;
-	  show_cpu_temperature = 1;
-	  column_meters_0 = "LeftCPUs2 GPU Memory Swap ZFSARC";
-	  column_meter_modes_0 = "1 1 1 1 2";
-	  column_meters_1 = "RightCPUs2 Tasks LoadAverage Uptime Hostname";
-	  column_meter_modes_1 = "1 2 2 2 2";
+          show_cpu_temperature = 1;
+          column_meters_0 = "LeftCPUs2 GPU Memory Swap ZFSARC";
+          column_meter_modes_0 = "1 1 1 1 2";
+          column_meters_1 = "RightCPUs2 Tasks LoadAverage Uptime Hostname";
+          column_meter_modes_1 = "1 2 2 2 2";
         };
       };
     };
