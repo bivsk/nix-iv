9b6cf388dbc85c8d97729e3721a2a3062e530c24
bivsk <bivsk@tutanota.com>
2025-09-17 21:56:15 -0400

    calibre: configure users and libraries

--- modules/nixos/server/nixarr/calibre.nix
+++ modules/nixos/server/nixarr/calibre.nix
@@ -3,6 +3,12 @@
     services.calibre-server = {
       enable = true;
       port = 4545;
+      auth = {
+        enable = true;
+        mode = "basic";
+        userDb = "/media/calibre/users.sqlite";
+      };
+      libraries = [ "/media/calibre" ];
     };
   };
 }
