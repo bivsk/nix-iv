55f7e88612fe87e3f9adb1658234aa43d50d49ab
bivsk <bivsk@tutanota.com>
2025-09-17 22:29:49 -0400

    sunny: update ssh key/secrets

--- modules/hosts/sunny/configuration.nix
+++ modules/hosts/sunny/configuration.nix
@@ -1,9 +1,8 @@
-{ lib, ... }:
 {
   flake.modules.nixos."nixosConfigurations/sunny" =
     { config, ... }:
     {
-      age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3TPAW2IIo4vwnSyoxEUrYM2SN44hXyIOdn2q0fdDW0";
+      age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAbSst9DGpCA4JFO+ftqtaTzxtULSkFi+fbXj/eoa3b/";
 
       networking.hostName = "sunny";
       networking.ipv4.address = "192.168.0.22";
