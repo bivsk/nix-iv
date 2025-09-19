af636c759fa5d31c219bfa085ed0ee12d797f48a
bivsk <bivsk@tutanota.com>
2025-09-19 01:05:37 -0400

    calibre: currently broken
    re-enable when https://github.com/NixOS/nixpkgs/issues/443794 is
    resolved

--- modules/nixos/server/nixarr/calibre.nix
+++ modules/nixos/server/nixarr/calibre.nix
@@ -1,14 +1,18 @@
 {
-  flake.modules.nixos.calibre = {
-    services.calibre-server = {
-      enable = true;
-      port = 4545;
-      auth = {
-        enable = true;
-        mode = "basic";
-        userDb = "/media/calibre/users.sqlite";
+  flake.modules.nixos.calibre =
+    let
+      calibreDir = "/media/nixarr/library/books/.calibre";
+    in
+    {
+      services.calibre-server = {
+        enable = false;
+        port = 4545;
+        auth = {
+          enable = true;
+          mode = "basic";
+          userDb = "${calibreDir}/users.sqlite";
+        };
+        libraries = [ calibreDir ];
       };
-      libraries = [ "/media/calibre" ];
     };
-  };
 }
