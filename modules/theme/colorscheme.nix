71f74f587da72b67d593b3f6ec43ae67390b1818
bivsk <bivsk@tutanota.com>
2025-10-01 12:28:01 -0400

    theme: kanagawa

--- modules/theme/colorscheme.nix
+++ modules/theme/colorscheme.nix
@@ -3,7 +3,7 @@
     { pkgs, ... }:
     let
       # https://github.com/tinted-theming/schemes/tree/spec-0.11/base16/
-      theme = "solarized-dark";
+      theme = "kanagawa";
     in
     {
       stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
