daa12b2c8d579d3f4d0b0197000fcb5b88028a1e
bivsk <bivsk@tutanota.com>
2025-10-15 17:20:54 -0400

    firefox: install bitwarden extension

--- modules/home/desktop/web-browsers/firefox.nix
+++ modules/home/desktop/web-browsers/firefox.nix
@@ -13,6 +13,11 @@
         DisableTelemetry = true;
         DontCheckDefaultBrowser = true;
         ExtensionSettings = {
+          # Bitwarden
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/{446900e4-71c2-419f-a6a7-df9c091e268b}/latest.xpi";
+          };
           # Metamask
           "webextension@metamask.io" = {
             installation_mode = "force_installed";
