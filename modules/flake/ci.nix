7b0740788e74def17ce5069353fe936fbb0178af
bivsk <bivsk@tutanota.com>
2026-02-12 02:45:42 -0500

    ci: configure flake-update effect

--- modules/flake/ci.nix
+++ modules/flake/ci.nix
@@ -4,11 +4,17 @@
 
   hercules-ci.flake-update = {
     enable = true;
+    baseMerge = {
+      enable = true;
+      method = "fast-forward";
+    };
+    autoMergeMethod = "merge";
+    pullRequestTitle = "chore: update flake.lock";
     when = {
-      hour = [ 3 ];
+      hour = [ 9 ]; # UTC
       dayOfWeek = [
-        "Mon"
-        "Wed"
+        "Tue"
+        "Thu"
         "Sat"
       ];
     };
