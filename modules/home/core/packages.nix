e4a75d1c2808f28ac8e0553b8bfa33245194ac0b
bivsk <bivsk@tutanota.com>
2026-02-19 22:52:41 -0500

    chore: formatting

--- modules/home/core/packages.nix
+++ modules/home/core/packages.nix
@@ -21,7 +21,7 @@
         };
         yazi = {
           enable = true;
-	  shellWrapperName = "y";
+          shellWrapperName = "y";
           settings = {
             mgr.show_hidden = true;
             open.rules = [
