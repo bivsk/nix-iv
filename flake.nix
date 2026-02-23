162971dffa95b9471f77cbca6082c3b16663d70c
bivsk <bivsk@tutanota.com>
2026-02-23 13:26:16 -0500

    openclaw: initialize

--- flake.nix
+++ flake.nix
@@ -56,6 +56,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-openclaw.url = "github:openclaw/nix-openclaw";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
